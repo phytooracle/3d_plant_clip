18d05c77de92e14313cde4401389366f85f6f59d
Travis-Simmons <920117874@student.ccga.edu>
2021-01-22 20:04:16 -0500

    incorperated plant isolation / above ground biomass extraction as a function, also exporting a csv of plant volumes in the original pcd

--- 3d_plant_clip.py
+++ 3d_plant_clip.py
@@ -17,6 +17,8 @@ import fiona
 import pyproj
 from shapely.ops import transform
 from shapely import geometry
+import statistics as stats
+from sklearn.cluster import KMeans
 import utm
 
 
@@ -130,6 +132,109 @@ def display_inlier_outlier(cloud, ind):
 
     inlier_cloud = cloud.select_down_sample(ind)
 
+#---------------------------------------------------
+def get_plant_vol(plant_pcd):
+
+    ar_whole_plant = np.asarray(plant_pcd)
+
+    # Find middle z
+    z_axis = []
+
+    for i in ar_whole_plant:
+        z_axis.append(i[2])
+
+    median_z = stats.median(z_axis)
+
+
+    # Slicing
+
+    slice_storage_list = []
+
+    a = ar_whole_plant[0][0]
+    cnt = 1
+
+    # Create one slice
+    for i in ar_whole_plant:
+        
+        
+        if (abs(i[0] - a) < 10) | (len(slice_storage_list) < 1):
+            slice_storage_list.append(list(i))
+            a = i[0]
+
+
+        # Cluster the slice, and add clusters that have  a point above the overall
+        # z to a 'plant list'
+        else:
+            # Drop y to flatten here
+            flat_slice = copy.deepcopy(slice_storage_list)
+            for j in flat_slice:
+                del j[1]
+            # Cluster here
+            km = KMeans(
+                n_clusters=5, init='random',
+                n_init=10, max_iter=300, 
+                tol=1e-04, random_state=0
+            )
+            
+            X = np.array(flat_slice)
+            y_km = km.fit_predict(X)
+
+            # Choose cluster
+            l0 = []
+            l1 = []
+            l2 = []
+            l3 = []
+            l4 = []
+            for index, row in enumerate(slice_storage_list):
+                # print(index, y_km[index], row)
+
+
+                if y_km[index] == 0:
+                    l0.append(row)
+                if y_km[index] == 1:
+                    l1.append(row)
+                if y_km[index] == 2:
+                    l2.append(row)
+                if y_km[index] == 3:
+                    l3.append(row)
+                if y_km[index] == 4:
+                    l4.append(row)
+
+            clusters = [l0,l1,l2,l3,l4]
+
+            
+
+            for c in clusters:
+                counter = 0
+                for i in c:
+                    # print(i)
+                    # Below worked ok
+
+                    # Tweak these two to get different outputs
+                    # best so far is is .01 and 20
+
+                    if i[2] > median_z + (median_z * .01):
+                    # if i[2] > median_z:
+                        counter += 1
+                    
+                    if counter >= (len(c)*.2):
+                        plant_points.extend(c)
+                        break
+
+
+
+            slice_storage_list = []
+
+            a = i[0]
+            # print(len(plant_points))
+            # print(f'Slice {cnt} done')
+            # cnt+=1
+
+    pcd_slice = o3d.geometry.PointCloud()
+    pcd_slice.points = o3d.utility.Vector3dVector(plant_points)
+    pcd_slice_bb = pcd_slice.get_axis_aligned_bounding_box()
+    plant_volumes_dict[plant_id] = pcd_slice_bb.volume()
+
 
 # --------------------------------------------------
 def main():
@@ -147,7 +252,10 @@ def main():
     if not os.path.isdir(args.outdir):
         os.makedirs(args.outdir)
 
+    plant_volumes_dict = {}
+
     for k, v in int_dict.items():
+
         plant_id = v['plant_id']
         plant_id = plant_id.strip('( )').replace("',", "").replace("'", "").replace(' ', '_')
         plot = v['plot']
@@ -157,6 +265,16 @@ def main():
 
         plant = create_bbox_polygon(min_x, max_y, max_x, min_y, pcd)
         o3d.io.write_point_cloud(out_file, plant)
+
+        get_plant_vol(plant)
+
+    plant_volumes_df = pd.DataFrame.from_dict(plant_volumes_dict, orient = 'index', columns = ['plant_vol'])
+
+    plant_volumes_df.to_csv(os.path.basename(args.pcd) + '_plant_vol.csv')
+
+
+
+
 
 
 # --------------------------------------------------
